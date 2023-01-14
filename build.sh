32d0705cf5cf2902e67d6d7ee77e5fbf1300f4c4
Rustam Abdumalikov <abdumali@login1.hpc.ut.ee>
2023-01-14 10:41:36 +0200

    with currect code achieved paper results, but not better

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu
+#SBATCH -J t5_scan
+#SBATCH -o ./megalog.out # STDOUT
+
+#SBATCH -t 48:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=20G
+#SBATCH --cpus-per-task=30
+#SBATCH --gres=gpu:a100-80g:1
+##SBATCH --gres=gpu:tesla:1
+
+echo "Started..."
+
+python -u t5_exp1b.py
+
+echo "Ended."
